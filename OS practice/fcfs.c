202e48ad06e3df58032cd9ab942a0ea6c094103f
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-11-27 23:29:51 +0530

    done

--- OS practice/fcfs.c
+++ OS practice/fcfs.c
@@ -1,16 +1,32 @@
 #include<stdio.h>
 int main(){
-    int n; 
     printf("Enter the no.of processes: "); 
-    scanf("%d", &n); 
-    int bt[n], wt[n], tat[n]; 
-    printf("Enter the burst times of each process:\n");
-    for(int i=0;i<n; i++){
-        printf("Process %d", i+1); 
+    int n; 
+    scanf("%d", &n);
+    int bt[n], tat[n], wt[n]; 
+    printf("Enter teh burst time of each process:\n");
+    for(int i=0; i<n; i++){{
+        process("Process %d", i+1)
         scanf("%d", &bt[i]); 
+    };
+    wt[o]=0; 
+    for(int i =1;i<n; i++){
+        wt[i]=wt[i-1]+bt[i-1];
     }
-    wt[0]=0;
-    for(int i=1;i<n;i++){
-        wt[i]=wt[i-1] + bt[i-1];[ ]
+    for(int i=0; i<n; i++){
+        tat[i]=wt[i]+bt[i]; 
     }
+    //average values; 
+    for(int i =0; i<n; i++){
+        avgWt+=wt[i];
+        avgTat+=tat[i];
+    }
+    float avgWt=0, avgTat=0;
+    avgWt=avgWt/n;
+    avgTat=avgTat/n;
+    printf("Process\tBurst Time\tWaiting Time\tTurnaround Time\n"); 
+    for(int i=0; i<n; i++){
+        printf("P%d\t%d\t\t%d\t\t%d\n", i+1, bt[i], wt[i], tat[i]);
+    }
+    return 0; 
 }
