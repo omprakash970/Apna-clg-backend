0a36435ea84705325f3a1b42589bb6476d476521
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-07 01:10:05 +0530

    done

---
+++ Miscllineous/MongoDB/index.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+let url="https://locohost:3000/users"; 
+mongoose.connect("mongodb://127.0.0.1:27017/students");
+main().then(()=>{
+    console.log("Connected to MongoDB");
+})
+
+.catch(err => console.log(err));
+async function main(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/students");
+}
